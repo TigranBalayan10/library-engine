4f2b2b60913a13a7bfaee74c9596cb736ac07da7
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-15 21:15:05 -0700

    add deletebook query

--- client/src/App.js
+++ client/src/App.js
@@ -14,20 +14,9 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
